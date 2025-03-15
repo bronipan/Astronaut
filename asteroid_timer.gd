4ae0798a14262cc3dba40e5e8a92c4294dfa1613
Bobronimir <pan.bronipan@gmail.com>
2025-03-15 21:58:03 +0100

    Zmensovani prekazek nahodne, nutno zmensovat rucne sprite a collisionbox

--- asteroid_timer.gd
+++ asteroid_timer.gd
@@ -7,12 +7,24 @@ var erzeugteAsteroiden: int
 func _on_timeout() -> void:
 	erzeugteAsteroiden+=1
 	var schwierigkeit= erzeugteAsteroiden/20
+	
+	# Vytvorit duplikat nahodneho potomka v nahodnem miste
 	var a=randi_range(1,get_child_count())
 	var neuerAsteroid:RigidBody2D=get_child(a-1).duplicate()
 	neuerAsteroid.visible=true
 	neuerAsteroid.position.x=1700
 	neuerAsteroid.position.y=randi_range(20,880)
+	
+	# Zmensovani objektu, nutno zmenit rucne velikost sprite i collisionshape, vsechno kruznice
+	var newScale=randf_range(0.5,1)
+	print("New object ",neuerAsteroid.name," scaled ",newScale)
+	neuerAsteroid.get_node("Sprite2D").scale*=Vector2(newScale,newScale)
+	neuerAsteroid.get_node("CollisionShape2D").shape = neuerAsteroid.get_node("CollisionShape2D").shape.duplicate()
+	neuerAsteroid.get_node("CollisionShape2D").shape.radius*=newScale
+	
+	# Zmenit rodice, aby se mi nehromadily u mne
 	asteroidenfeld.add_child(neuerAsteroid)
+	
 	neuerAsteroid.apply_impulse(Vector2(-randf_range(300+schwierigkeit*50,
 		600+schwierigkeit*100),0))
 	Ereignisse.emit_signal("punkte",(schwierigkeit+1)*10)
--- astronaut.gd
+++ astronaut.gd
@@ -20,6 +20,11 @@ func _process(delta: float) -> void:
 	
 
 func _on_body_entered(body: Node2D) -> void:
+	if body.get_meta("typ") == "goldmunze":
+		Ereignisse.emit_signal("punkte",1000)
+		body.queue_free()
+		return
+
 	Ereignisse.emit_signal("gameover")
 	hide()
 	$CollisionPolygon2D.set_deferred("disabled",true)
@@ -28,5 +33,6 @@ func _on_body_entered(body: Node2D) -> void:
 func _on_start():
 	show()
 	position.y= get_viewport_rect().get_center().y
+	position.x=50
 	$CollisionPolygon2D.set_deferred("disabled",false)
 	
